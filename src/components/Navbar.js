de68bbe5530b2b288a343e5bda4d66fbc9338c70
veyseltosun <postalarim1923@gmail.com>
2023-01-17 20:17:23 +0300

    ...

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,7 +11,7 @@ const Navbar = () => {
     return(
         <nav className="ui raised very padded segment">
             <a className="ui teal inverted segment" href="https://reactrouter.com/en/main/upgrading/v5">
-             Go to React Router V6   <i className="hand point left outline icon"></i>
+             Router V6   <i className="hand point left outline icon"></i>
             </a>
             <div className="ui right floated header">
                 <button className="ui button" ><Link to="/">Home</Link></button>
