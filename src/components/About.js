5ff2b4796f3cb1b9465ed0191244c3069d5e1bc4
veyseltosun <postalarim1923@gmail.com>
2023-01-17 03:19:11 +0300

    ....

---
+++ src/components/About.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const About = () => {
+    return (
+        <div
+            className="ui raised very padded text container segment"
+            style={{ marginTop: "80px", }}
+        >
+            <h3 className="ui header">About</h3>
+            <p>Lorem ipsum sit amet dolar.</p>
+        </div>
+    )
+}
+export default About ;
---
+++ src/components/Home.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Home = () => {
+    return (
+        <div
+            className="ui raised very padded text container segment"
+            style={{ marginTop: "80px", }}
+        >
+            <h3 className="ui header">Home</h3>
+            <p>Lorem ipsum sit amet dolar.</p>
+        </div>
+    )
+}
+export default Home ;
